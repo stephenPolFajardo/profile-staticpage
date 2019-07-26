7be65676a731743d7a332d1f1b137a59d169b41b
StephenPol <52106088+stephenPolFajardo@users.noreply.github.com>
2019-07-26 14:46:21 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function myFunction() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
@@ -11,5 +12,38 @@ function myFunction() {
     dots.style.display = "none";
     btnText.innerHTML = "Read less";
     moreText.style.display = "inline";
+  }
+}
+
+function openObj(evt, objName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(objName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function onLoad() {
+  var parent = document.getElementById('Skills');
+  for (i = 0; i < skill.length; i++){
+    var element = document.createElement('div');
+    var element2 = document.createElement('div');
+    var element3 = document.createElement('div');
+    var int = skill[i].level;
+    element2.setAttribute('id','myProgress');
+    element.setAttribute('class', 'column');
+    element3.setAttribute('id', 'myBar');
+    element3.style.width = parseInt(skill[i].level)+"%";
+    element3.innerHTML = skill[i].level+"%";
+    element2.innerHTML = skill[i].name;
+    document.getElementById('Skills').appendChild(element);
+    element.append(element2);
+    element2.append(element3);
   }
 }
