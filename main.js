4b387f1d5afd0f864fc692bb5e389f32847ac09d
StephenPol <52106088+stephenPolFajardo@users.noreply.github.com>
2019-08-07 21:23:47 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -31,20 +31,59 @@ function openObj(evt, objName) {
 
 function onLoad() {
   var parent = document.getElementById('Skills');
-  for (i = 0; i < skill.length; i++){
+  for (var i = skill.length -1; i >= 0; i--){
+    reverse = skill[i].name.split("").reverse().join("");
+    console.log(reverse);
     var element = document.createElement('div');
     var element2 = document.createElement('div');
     var element3 = document.createElement('div');
+    var element4 = document.createElement('img');
+    var element5 = document.createElement('div');
     var int = skill[i].level;
+    var imgPath = skill[i].path;
     element2.setAttribute('id','myProgress');
     element.setAttribute('class', 'column');
     element3.setAttribute('id', 'myBar');
     element3.setAttribute('class', 'line');
     element3.style.maxWidth = parseInt(skill[i].level)+"%";
-    element3.innerHTML = skill[i].level+"%";
+    element4.setAttribute('src', imgPath);
+    element4.setAttribute('class', 'img-skill');
+    element5.setAttribute('id', 'back-color');
+    var grade = skill[i].level;
+    if(grade >= 90){
+      element3.innerHTML = skill[i].level+"%" + " " + "Excellent";
+    }
+    else if(grade >= 80){
+      element3.innerHTML = skill[i].level+"%" + " " + "Very good";
+    }
+    else if(grade >= 70){
+      element3.innerHTML = skill[i].level+"%" + " " + "Good";
+    }
+    else if(grade >= 60){
+      element3.innerHTML = skill[i].level+"%" + " " + "Satisfactory";
+    }
+    else if(grade >= 50){
+      element3.innerHTML = skill[i].level+"%" + " " + "Sufficient";
+    }
+    else if(grade >= 1){
+      element3.innerHTML = skill[i].level+"%" + " " + "Insufficient";
+    }
+
     element2.innerHTML = skill[i].name;
     document.getElementById('Skills').appendChild(element);
     element.append(element2);
-    element2.append(element3);
+    element2.append(element4);
+    element5.append(element3);
+    element2.append(element5);
   }
 }
+
+// function myReverse(arr) {
+//   var array = document.getElementById('Skills');
+//   var newArray = [];
+//   for (i = array.length -1; i >= 0; i--){
+//     newArray.push(arr[i])
+//   }
+//   return newArray;
+//   console.log('sssss',newArray);
+// }
